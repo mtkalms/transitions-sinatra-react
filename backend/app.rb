945856b458329821fb693f17061bb53c3ff7fa68
Matthias Kalms <mtkalms@gmail.com>
2025-08-20 04:19:26 +0000

    Format project

--- backend/app.rb
+++ backend/app.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 require 'sinatra'
 require 'sinatra/activerecord'
 
-Dir.glob('./{models,controllers}/*.rb').each { |file| require file }
+Dir.glob('./{models,controllers}/*.rb').sort.each { |file| require file }
 
 get '/' do
   "Welcome to the Ruby side, where there's no one else but me."
