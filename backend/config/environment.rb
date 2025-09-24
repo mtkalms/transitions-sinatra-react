b1e854d737aedc8c4716ba40ad277bbfede30703
Matthias Kalms <mtkalms@gmail.com>
2025-09-24 03:32:47 +0000

    Fix public path

--- backend/config/environment.rb
+++ backend/config/environment.rb
@@ -9,4 +9,4 @@ Dir["#{root}/app/controllers/*.rb"].sort.each { |f| require f }
 
 # Set views path for all controllers if desired:
 Sinatra::Base.set :views, File.expand_path('../app/views', __dir__)
-Sinatra::Base.set :public_folder, File.expand_path('../public', __dir__)
+Sinatra::Base.set :public_folder, File.expand_path('../app/public', __dir__)
